54e636e0bbf1a1c729714479828988a9f3fe54ac
Prajwal-B05 <prajwal.is20@bmsce.ac.in>
2023-05-17 11:00:49 +0530

    Declared the Constructor function that contains the address array  and uint value called requiredSig that is required to transfer the funds

---
+++ Contracts/MultiISig.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+contract MultiSig {
+    //State variables
+    address[] public owners;
+    uint txnID;
+    uint public required;
+    struct Transaction {
+        address destination;
+        uint value;
+        bool executed;
+    }
+    mapping(uint => mapping(address => bool)) public confirmations;
+    Transaction[] public transactions;
+
+    //Contructor function
+    constructor(address[] memory x, uint y) {
+        require(x.length >= 0);
+        require(y > 0);
+        require(y < x.length);
+        owners = x;
+        required = y;
+    }
+}
