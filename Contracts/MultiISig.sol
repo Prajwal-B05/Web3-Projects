2308e5ebfaae7a314942081df64b4beffba5fc33
Prajwal-B05 <prajwal.is20@bmsce.ac.in>
2023-05-18 22:22:52 +0530

    - added function to add the transaction and count the transactions as well . - added function to confirm the transaction by passing the  Transaction Id.

--- Contracts/MultiISig.sol
+++ Contracts/MultiISig.sol
@@ -6,20 +6,39 @@ contract MultiSig {
     address[] public owners;
     uint txnID;
     uint public required;
+    uint transactionCount ;
     struct Transaction {
         address destination;
         uint value;
         bool executed;
+        bytes data ;
     }
     mapping(uint => mapping(address => bool)) public confirmations;
     Transaction[] public transactions;
 
     //Contructor function
-    constructor(address[] memory x, uint y) {
+    constructor(address[] memory x, uint y) 
+    {
         require(x.length >= 0);
         require(y > 0);
         require(y < x.length);
         owners = x;
-        {
     }
+    
+        function addTransaction(address payable destination, uint value, bytes memory data ) public returns(uint) 
+        {
+        transactions[transactionCount] = Transaction(destination, value, false ,data);
+        transactionCount += 1;
+        return transactionCount - 1;
+        }
+
+        function confirmTransaction(uint transactionId) public {
+        require(isOwner(msg.sender));
+        confirmations[transactionId][msg.sender] = true;
+        if(isConfirmed(transactionId)) {
+            executeTransaction(transactionId);
+        }
+    }
+
+   
 }
