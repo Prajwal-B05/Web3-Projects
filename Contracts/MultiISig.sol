fcaa5cfda5bc0c5fffde294c26de6fe038fb5d97
Prajwal-B05 <prajwal.is20@bmsce.ac.in>
2023-05-18 22:35:51 +0530

    - added the function to check  the calling address is owner or not . - added the function to execute the transaction immediately confirm the transfer of funds once the required confirmations are met . - added the function to count the number of confirmations by the owners.

--- Contracts/MultiISig.sol
+++ Contracts/MultiISig.sol
@@ -1,29 +1,42 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-contract MultiSig {
-    //State variables
-    address[] public owners;
-    uint txnID;
-    uint public required;
-    uint transactionCount ;
-    struct Transaction {
+    contract MultiSig {
+     //State variables
+     address[] public owners;
+     uint txnID;
+     uint public required;
+     uint transactionCount ;
+     struct Transaction 
+     {
         address destination;
         uint value;
         bool executed;
         bytes data ;
-    }
-    mapping(uint => mapping(address => bool)) public confirmations;
-    Transaction[] public transactions;
+     }
+     mapping(uint => mapping(address => bool)) public confirmations;
+     Transaction[] public transactions;
 
-    //Contructor function
-    constructor(address[] memory x, uint y) 
-    {
+      //Contructor function
+      constructor(address[] memory x, uint y) 
+       {
         require(x.length >= 0);
         require(y > 0);
         require(y < x.length);
         owners = x;
-    }
+        }
+        
+        function isOwner (address addr) private view returns(bool) 
+        {
+        for(uint i = 0; i < owners.length; i++) 
+        {
+            if(owners[i] == addr) 
+            {
+                return true;
+            }
+        }
+        return false;
+        }
     
         function addTransaction(address payable destination, uint value, bytes memory data ) public returns(uint) 
         {
@@ -32,13 +45,37 @@ contract MultiSig {
         return transactionCount - 1;
         }
 
-        function confirmTransaction(uint transactionId) public {
+        
+      function isConfirmed(uint transactionId) public view returns(bool) 
+      {
+        return getConfirmationsCount(transactionId) >= required;
+      }
+
+
+        function confirmTransaction(uint transactionId) public 
+        {
         require(isOwner(msg.sender));
         confirmations[transactionId][msg.sender] = true;
-        if(isConfirmed(transactionId)) {
+        if(isConfirmed(transactionId)) 
+          {
             executeTransaction(transactionId);
+          }
         }
+
+        function getConfirmationsCount(uint transactionId) public view returns(uint) 
+        {
+        uint count;
+        for(uint i = 0; i < owners.length; i++) 
+          {
+            if(confirmations[transactionId][owners[i]]) 
+            {
+                count++;
+            }
+          }
+        return count;
+        }
+
     }
 
    
-}
+
